b15187faa9e430080c10f7df5375ce15f86c2c0f
trungkhanh2709 <111423000+trungkhanh2709@users.noreply.github.com>
2025-07-23 01:47:44 +0700

    update

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,21 +1,21 @@
 const chromium = require("chrome-aws-lambda");
-const puppeteer = require("puppeteer-core");
+const puppeteer = require("puppeteer");
 
 const scrapeWebsite = async (url) => {
   let browser = null;
   try {
-    browser = await chromium.puppeteer.launch({
+    const executablePath = await chromium.executablePath || '/usr/bin/google-chrome'; // fallback cho local
+    browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
+      executablePath,
+      headless: true,
     });
 
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "domcontentloaded", timeout: 30000 });
 
     const content = await page.content();
-    const cheerio = require("cheerio");
     const $ = require("cheerio").load(content);
     const text = $("body").text();
     return text.replace(/\s+/g, " ").trim().slice(0, 15000);
