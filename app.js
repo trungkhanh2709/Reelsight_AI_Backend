994dfeae8201ac08f8ed5e9af9313d93ddce3513
trungkhanh2709 <111423000+trungkhanh2709@users.noreply.github.com>
2025-07-22 23:34:51 +0700

    fix cors

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.URL_FRONTEND, // Thay bằng URL Netlify thực tế
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Import routes
 const agentRoute = require("./routes/agent");
@@ -14,5 +20,4 @@ const agentReportRoute = require("./routes/report");
 app.use("/api", agentRoute);
 app.use("/api", agentChatRoute);
 app.use("/api", agentReportRoute);
-
 app.listen(5000, () => console.log("Server listening on port 5000"));
